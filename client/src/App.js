7138c6afaa1f0d0c2a76b4b257634cd92c0b4b6b
Jaxon Porter <jaxonutah@gmail.com>
2019-03-27 16:13:28 -0600

    authentication and tokens work

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home'
 import NoMatch from './components/NoMatch'
 import Login from './components/Login'
 import Register from './components/Register'
-import logo from './logo.svg';
+import FetchUser from './components/FetchUser'
 import './App.css';
 
 class App extends Component {
@@ -13,12 +13,14 @@ class App extends Component {
     return (
       <>
       <Navbar />
+      <FetchUser>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
         <Route component={NoMatch} />
       </Switch>
+      </FetchUser>
       </>
       
     )
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import { AuthProvider, } from './providers/AuthProvider';
 import { BrowserRouter, } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css'
+import { initMiddleware, } from 'devise-axios'
 import * as serviceWorker from './serviceWorker';
+
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
